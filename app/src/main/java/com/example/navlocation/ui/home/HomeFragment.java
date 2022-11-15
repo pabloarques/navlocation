65dee7c12b2dc92d010d5adade5948fcd6211904
pabarqlop <pabloarques98@gmail.com>
2022-11-15 14:00:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/navlocation/ui/home/HomeFragment.java
@@ -0,0 +1,103 @@
+package com.example.navlocation.ui.home;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import com.example.navlocation.databinding.FragmentHomeBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+    private Location mLastLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private ActivityResultLauncher<String[]> locationPermissionRequest;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        //boton
+        binding.buttonlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLocation();
+            }
+        });
+
+        //definir codigo
+        locationPermissionRequest = registerForActivityResult(new ActivityResultContracts
+                        .RequestMultiplePermissions(), result -> {
+                    Boolean fineLocationGranted = result.getOrDefault(
+                            Manifest.permission.ACCESS_FINE_LOCATION, false);
+                    Boolean coarseLocationGranted = result.getOrDefault(
+                            Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                    if (fineLocationGranted != null && fineLocationGranted) {
+                        getLocation();
+                    } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                        getLocation();
+                    } else {
+                        Toast.makeText(requireContext(), "No concedeixen permisos", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        //inicializar mFused
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        final TextView textView = binding.localitzacio;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+        private void getLocation() {
+            if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+            Toast.makeText(requireContext(), "Request permisssions", Toast.LENGTH_SHORT).show();
+            locationPermissionRequest.launch(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        } else {
+            Toast.makeText(requireContext(), "getLocation: permissions granted", Toast.LENGTH_SHORT).show();
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(
+                    location -> {
+                        if (location != null) {
+                            mLastLocation = location;
+                            binding.localitzacio.setText(
+                                    String.format("Latitud: %1$.4f \n Longitud: %2$.4f\n Hora: %3$tr",
+                                            mLastLocation.getLatitude(),
+                                            mLastLocation.getLongitude(),
+                                            mLastLocation.getTime()));
+                        } else {
+                            binding.localitzacio.setText("Sense localització coneguda");
+                        }
+                    });
+        }
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
