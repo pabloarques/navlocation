0705c367b99aac2e47d82d5b9af98cf96cc08901
pabarqlop <pabloarques98@gmail.com>
2022-11-23 13:04:15 +0100

    lista hecha / localizacion no la pone

--- app/src/main/java/com/example/navlocation/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/navlocation/ui/dashboard/DashboardFragment.java
@@ -9,25 +9,97 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.navlocation.databinding.FragmentDashboardBinding;
+import com.example.navlocation.databinding.FragmentNotificationsBinding;
+import com.example.navlocation.databinding.IncidenciasBinding;
+import com.example.navlocation.ui.Incidencia;
+import com.example.navlocation.ui.SharedViewModel;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private FirebaseUser authUser;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        SharedViewModel sharedViewModel = new ViewModelProvider(
+                requireActivity()
+        ).get(SharedViewModel.class);
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+       sharedViewModel.getUser().observe(getViewLifecycleOwner(), firebaseUser -> {
+           authUser = firebaseUser;
+       if (firebaseUser != null){
+           DatabaseReference base = FirebaseDatabase.getInstance().getReference();
+           DatabaseReference users = base.child("users");
+           DatabaseReference uid = users.child(authUser.getUid());
+           DatabaseReference incidencias = uid.child("incidencies");
+
+           FirebaseRecyclerOptions<Incidencia> options = new FirebaseRecyclerOptions.Builder<Incidencia>()
+                   .setQuery(incidencias, Incidencia.class)
+                   .setLifecycleOwner(this)
+                   .build();
+
+            IncidenciaAdapter adapter = new IncidenciaAdapter(options);
+           binding.rvIncidencies.setAdapter(adapter);
+           binding.rvIncidencies.setLayoutManager(
+           new LinearLayoutManager(requireContext())
+                );
+
+       }
+
+       });
+
+
+
+        View root = binding.getRoot();
         return root;
     }
+
+    class IncidenciaAdapter extends FirebaseRecyclerAdapter<Incidencia, IncidenciaAdapter.IncidenciaViewholder> {
+
+        public IncidenciaAdapter(@NonNull FirebaseRecyclerOptions<Incidencia> options) {
+            super(options);
+        }
+
+        @Override
+        protected void onBindViewHolder(@NonNull IncidenciaAdapter.IncidenciaViewholder holder, int position, @NonNull Incidencia model) {
+            holder.binding.txtDescripcio.setText(model.getProblema());
+            holder.binding.txtAdreca.setText(model.getDireccio());
+        }
+
+
+        @NonNull
+        @Override
+        public IncidenciaViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType
+            ) {
+            return new IncidenciaViewholder(IncidenciasBinding.inflate(
+                    LayoutInflater.from(parent.getContext()),
+                    parent, false));
+        }
+
+        class IncidenciaViewholder extends RecyclerView.ViewHolder {
+            IncidenciasBinding binding;
+
+            public IncidenciaViewholder(IncidenciasBinding binding) {
+                super(binding.getRoot());
+                this.binding = binding;
+            }
+        }
+    }
+
+
 
     @Override
     public void onDestroyView() {
